348ea1cf70756e1b71f8ccc31e54e841d07839eb
Benjamin Paillereau <bpaillereau@gmail.com>
2013-06-13 10:35:50 +0200

    fixes on juzu 0.6.0

--- juzu-sample/src/main/java/name/Controller.java
+++ juzu-sample/src/main/java/name/Controller.java
@@ -1,38 +1,28 @@
 package {{name}};
 
-import juzu.Path;
-import juzu.View;
+import juzu.*;
 import juzu.template.Template;
-
+import juzu.request.RenderContext;
 import javax.inject.Inject;
 import javax.portlet.PortletPreferences;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /** @author <a href="mailto:benjamin.paillereau@exoplatform.com">Benjamin Paillereau</a> */
 public class Controller
 {
-
    /** . */
    @Inject
    @Path("index.gtmpl")
    Template indexTemplate;
 
-
    @Inject
    PortletPreferences portletPreferences;
 
-
    @View
-   public void index() throws IOException
+   public Response.Content index(RenderContext renderContext)
    {
-
       String size = portletPreferences.getValue("size", "128");
        
-      Map<String, Object> parameters = new HashMap<String, Object>();
-      parameters.put("size", size);
-      indexTemplate.render(parameters);
+      return indexTemplate.with().set("size", size).ok();
    }
 
 }
--- juzu-sample/src/main/java/name/package-info.java
+++ juzu-sample/src/main/java/name/package-info.java
@@ -1,4 +1,5 @@
 @Assets(
+        location = AssetLocation.SERVER,
         scripts = {
                 @Script(src = "js/jquery-1.8.3.min.js", id = "jquery")
         }
@@ -8,6 +9,7 @@
 @Portlet package {{name}};
 
 import juzu.Application;
+import juzu.asset.AssetLocation;
 import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
 import juzu.plugin.portlet.Portlet;
